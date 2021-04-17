4703815be32ca303fa5243dede9c8e00becc3d2a
User <User@HOME-PC>
2021-04-17 23:32:33 +0300

    Интерфейсы SFX, GameSoundPlayer, Unit2D

---
+++ SFXMusic/GameSoundPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFXMusic
+{
+    public interface IGameSoundPlayer
+    {
+        IEnumerable<ISFX> SFXList { get; set; }
+        void Play();
+        void Pause();
+        void Stop();
+    }
+}
